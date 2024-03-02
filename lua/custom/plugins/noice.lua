f3eacc41677cbd1feea26a78bddfb42963762b1f
Roconx <roconx@proton.me>
2024-03-02 12:12:48 +0100

    Add noice

---
+++ lua/custom/plugins/noice.lua
@@ -0,0 +1,18 @@
+-- Might remove later
+return {
+  'folke/noice.nvim',
+  event = 'VeryLazy',
+  opts = {
+    presets = {
+      bottom_search = true, -- use a classic bottom cmdline for search
+    },
+  },
+  dependencies = {
+    -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+    'MunifTanjim/nui.nvim',
+    -- OPTIONAL:
+    --   `nvim-notify` is only needed, if you want to use the notification view.
+    --   If not available, we use `mini` as the fallback
+    'rcarriga/nvim-notify',
+  },
+}
