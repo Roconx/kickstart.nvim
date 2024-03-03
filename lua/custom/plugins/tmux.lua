6034460fac1f0819627468a3a4c00406c6b005ad
Roconx <roconx@proton.me>
2024-03-03 09:16:51 +0100

    Add mode to keybindings

--- lua/custom/plugins/tmux.lua
+++ lua/custom/plugins/tmux.lua
@@ -3,10 +3,10 @@ return {
     'christoomey/vim-tmux-navigator',
     lazy = false,
     keys = {
-      { 'n', '<C-h>', '<cmd> TmuxNavigateLeft<CR>', desc = 'Go to left window', remap = true },
-      { 'n', '<C-l>', '<cmd> TmuxNavigateRight<CR>', desc = 'Go to lower window', remap = true },
-      { 'n', '<C-j>', '<cmd> TmuxNavigateDown<CR>', desc = 'Go to upper window', remap = true },
-      { 'n', '<C-k>', '<cmd> TmuxNavigateUp<CR>', desc = 'Go to right window', remap = true },
+      { mode = 'n', '<C-h>', '<cmd> TmuxNavigateLeft<CR>', desc = 'Go to left window', remap = true },
+      { mode = 'n', '<C-l>', '<cmd> TmuxNavigateRight<CR>', desc = 'Go to lower window', remap = true },
+      { mode = 'n', '<C-j>', '<cmd> TmuxNavigateDown<CR>', desc = 'Go to upper window', remap = true },
+      { mode = 'n', '<C-k>', '<cmd> TmuxNavigateUp<CR>', desc = 'Go to right window', remap = true },
     },
   },
 }
